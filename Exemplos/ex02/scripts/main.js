ee5b07569b4a2db915220c0584a88375b22e9b9a
Anderson Coelho <anderson.coelho1969@gmail.com>
2020-03-16 17:56:17 -0400

    Adicionando os exercicios feitos em sala.

---
+++ Exemplos/ex02/scripts/main.js
@@ -0,0 +1,15 @@
+/*
+    Declaração / Atribuição
+var nome; // Declaração
+nome = 'Anderson'; // Atribuição
+*/
+
+var nome = "Abner";
+console.log("Meu nome é: ", nome);
+
+nome = 'Antônio';
+
+//Entrada dinâmica
+var sanduiche = prompt("Qual o sanduiche voce gosta: ");
+
+console.log(sanduiche);
