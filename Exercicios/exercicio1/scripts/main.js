9126c87d0cc3390f25a1361d2d38ef482544ae06
Anderson Coelho <anderson.coelho1969@gmail.com>
2020-03-17 17:35:04 -0400

    Correção de alguns erros e adição de exercicíos propostos.

---
+++ Exercicios/exercicio1/scripts/main.js
@@ -0,0 +1,13 @@
+//Declarando e atribuindo
+var a = 'casa';
+var b = 'bicicleta';
+//Mostrando as entradas
+console.log("Primeiro numero: " + a);
+console.log("Segundo numero: " + b);
+//Fazendo a troca
+var aux = a;
+a = b;
+b = aux;
+//Mostrando a saída
+console.log("Primeiro numero trocado: " + a);
+console.log("Segundo numero trocado: " + b);
---
+++ Exercicios/exercicio2/scripts/main.js
@@ -0,0 +1,5 @@
+var numero = parseInt(prompt("Informe o numero: "));
+
+console.log("Numero: ", numero);
+console.log("Sucessor: ", numero + 1);
+console.log("Antecessor: ", numero - 1);
